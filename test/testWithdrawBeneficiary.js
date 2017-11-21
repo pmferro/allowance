07f48e31c1583e58ed26e4ec83e5161aebee3fd4
Pablo M. Ferro <pmferro@gmail.com>
2017-11-21 20:00:10 -0300

    version1

--- test/testWithdrawBeneficiary.js
+++ test/testWithdrawBeneficiary.js
@@ -1,11 +1,26 @@
 var Web3 = require('web3');
-var web = new Web3('http://localhost:8545');
+var web3 = new Web3('http://localhost:8545');
 var json = require('./abi.json');
 
-var contractAddress = "0x04788cb71bcb7d98ca1af624d7ff8a9d500fcc98c95e73bfe6e5530b4752c8bd"
-var beneficiaryAddress = "0xa1b115bdf19bf2312497c8ce7792138e69f51af6"
+var contractAddress = '0x6658d0841c7938f05c3fa6faedcbd2678780caa1'
+var beneficiaryAddress = '0xa1b115bdf19bf2312497c8ce7792138e69f51af6'
 
-var contract = new web.eth.Contract(json, contractAddress, beneficiaryAddress, {from: beneficiaryAddress, gasPrice: 20000000000});
+var contract = new web3.eth.Contract(json, contractAddress, beneficiaryAddress, {from: beneficiaryAddress, gasPrice: 20000000000});
 
-contract.methods.withdrawBeneficiary().send({from:beneficiaryAddress}).then(function(receipt){});
+console.log(contract.methods.withdrawBeneficiary().encodeABI());
 
+/*
+contract.methods.withdrawBeneficiary().call({from: beneficiaryAddress}, function(error, result){
+    console.log(web3.eth.getBalance(web3.eth.accounts[0]));
+    console.log(web3.eth.getBalance(web3.eth.accounts[1]));
+});
+*/
+
+contract.methods.withdrawBeneficiary().send({from: beneficiaryAddress})
+.then(function(receipt){
+    console.log(web3.eth.getBalance(web3.eth.accounts[0]));
+    console.log(web3.eth.getBalance(web3.eth.accounts[1]));    
+});
+
+
+//console.log(contract)
