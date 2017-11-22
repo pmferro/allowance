4056289bc369d71432c49b71ac7b981630b55f2b
Pablo M. Ferro <pmferro@gmail.com>
2017-11-21 21:31:30 -0300

    version1

--- test/testWithdrawBeneficiary.js
+++ test/testWithdrawBeneficiary.js
@@ -2,18 +2,18 @@ var Web3 = require('web3');
 var web3 = new Web3('http://localhost:8545');
 var json = require('./abi.json');
 
-var contractAddress = '0xb8bc902c71075038fac266b894d8b817e745cddd'
+var contractAddress = '0x11e3c7be68bb4bb080c7d8ec8cbe10db77545619'
 var beneficiaryAddress = '0xa1b115bdf19bf2312497c8ce7792138e69f51af6'
 
 var contract = new web3.eth.Contract(json, contractAddress, beneficiaryAddress, {from: beneficiaryAddress, gasPrice: 20000000000});
 
-console.log(contract.methods.withdrawBeneficiary().encodeABI());
+// console.log(contract.methods.withdrawBeneficiary().encodeABI());
 
-/*
+
 contract.methods.testContract().call({from: '0xa1b115bdf19bf2312497c8ce7792138e69f51af6'}, function(error, result){
     console.log(result)
 });
-*/
+
 
 
 contract.methods.withdrawBeneficiary().send({from: beneficiaryAddress})
