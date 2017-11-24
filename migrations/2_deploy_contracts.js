4403cc84417a5d70ffd779513a3b8902926cdfca
martingofox <martin.gomez@fox.com>
2017-11-24 14:28:31 -0300

    version2

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
-var Allowance = artifacts.require("./Allowance.sol");
+var Allowance = artifacts.require("Allowance");
 
-var beneficiaryAddress = "0x6bbbda4ec2fc2061fbf91ecd908eaf02cc7817a1"
+var beneficiaryAddress = "0xa46aef095c464745e38b63437341093932f8c074"
 
 module.exports = function(deployer) {
   deployer.deploy(Allowance, beneficiaryAddress, {value:5000000000000000000});
