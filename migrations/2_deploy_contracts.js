a5ccd4009c31f129834eee942ae7bd949c943569
Pablo Miguel Ferro <32720173+pmferro@users.noreply.github.com>
2017-12-07 13:58:37 -0300

    Update 2_deploy_contracts.js
    CryptoDaddy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
 //var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var Allowance = artifacts.require("./Allowance.sol");
 
-var beneficiaryAddress = "0xf082224628d18891c4fed12e8b223ec03452e3a5"
+//var beneficiaryAddress = "0x24bbf1b589eac6aa73562dce4be52a376068a5bf"
 
 module.exports = function(deployer) {
   //deployer.deploy(SimpleStorage);
-  deployer.deploy(Allowance, beneficiaryAddress, {value:1000000000000000000});
+  deployer.deploy(Allowance);
 };
